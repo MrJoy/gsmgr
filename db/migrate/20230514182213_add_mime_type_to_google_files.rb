fd475a8f9784d3e70140c6bd810a071ed904fac9
Jon Frisby <jfrisby@mrjoy.com>
2023-05-14 11:31:51 -0700

    Remove lint.

--- db/migrate/20230514182213_add_mime_type_to_google_files.rb
+++ db/migrate/20230514182213_add_mime_type_to_google_files.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMimeTypeToGoogleFiles < ActiveRecord::Migration[7.0]
   def change
     add_column :google_files, :mime_type, :string, null: false, default: ""
