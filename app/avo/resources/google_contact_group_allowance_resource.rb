9323ff7972772252f4b4092ed6cb18a3e8f8b1fe
Jon Frisby <jfrisby@mrjoy.com>
2023-05-21 16:58:03 -0700

    Streamline allowance workflow.

--- app/avo/resources/google_contact_group_allowance_resource.rb
+++ app/avo/resources/google_contact_group_allowance_resource.rb
@@ -19,8 +19,12 @@ class GoogleContactGroupAllowanceResource < Avo::BaseResource
         attach_scope: lambda {
                         # N.B. We _want_ this to blow up if someone is trying to create an
                         # allowance without first pinning down the contact group!
+                        #
+                        # Also, we only want to show root-level folders as options.
                         query
-                          .where(google_account_id: parent.contact_group.account.id)
+                          .where(google_account_id: parent.contact_group.account.id,
+                                 parent_id:         nil,
+                                 mime_type:         "application/vnd.google-apps.folder")
                           .order(name: "ASC")
                       }
 end
