9f8501725601390ea097043630bc3e329d0926f8
Jon Frisby <jfrisby@mrjoy.com>
2023-05-21 17:11:53 -0700

    Begin sorting out logic to determine, file-by-file, what permissions _should_ be.

--- app/models/google_file.rb
+++ app/models/google_file.rb
@@ -68,4 +68,70 @@ class GoogleFile < ApplicationRecord
   scope :anyone_with_link, -> do
     where(id: GoogleFilePermission.where(google_id: "anyoneWithLink").pluck(:google_file_id))
   end
+
+  def link_permission
+    permissions.where(target_type: "anyone").first
+  end
+
+  def link_access
+    link_permission&.role
+  end
+
+  def root_folder
+    root = parent
+    next_parent = root
+    while next_parent
+      next_parent = next_parent.parent
+      root = next_parent if next_parent
+    end
+    root
+  end
+
+  def effective_allowances
+    root_folder&.allowances || allowances
+  end
+
+  ACCESS_LEVELS = {
+    "reader" => 0,
+    "commenter" => 1,
+    "writer" => 2,
+    "owner" => 3,
+  }
+  ACCESS_LEVELS_REVERSE = ACCESS_LEVELS.invert
+  def expected_access_levels
+    result = {}
+    effective_allowances.includes(contact_group: :contacts).each do |allowance|
+      allowance.contact_group.contacts.each do |contact|
+        lvl = ACCESS_LEVELS[allowance.access_level]
+        result[contact.primary_email] ||= 0
+        result[contact.primary_email] = lvl if lvl > result[contact.primary_email]
+      end
+    end
+    result
+      .to_a
+      .map { |(email, lvl)| [email, ACCESS_LEVELS_REVERSE[lvl]] }
+      .sort
+  end
+
+  def current_access_levels
+    permissions
+      .where(target_type: "user",
+             role:       %w[reader commenter writer])
+      .pluck(:email_address, :role)
+      .sort
+  end
+
+  def access_level_changes
+    result = {}
+    current_access_levels.each do |(email, role)|
+      result[email] = [role, nil]
+    end
+
+    expected_access_levels.each do |(email, role)|
+      result[email] ||= [nil, nil]
+      result[email][1] = role
+    end
+
+    result.to_a.select { |(user, (from, to))| from != to }.sort
+  end
 end
